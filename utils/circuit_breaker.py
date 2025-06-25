193b257e5f9ca577a2d488977016d364669b6114
eharnischStEdword <eharnisch@stedward.org>
2025-06-25 12:13:46 -0500

    Create circuit_breaker.py

---
+++ utils/circuit_breaker.py
@@ -0,0 +1,30 @@
+# utils/circuit_breaker.py
+from datetime import datetime, timedelta
+
+class CircuitBreaker:
+    def __init__(self, failure_threshold=5, recovery_timeout=300):
+        self.failure_threshold = failure_threshold
+        self.recovery_timeout = recovery_timeout
+        self.failure_count = 0
+        self.last_failure_time = None
+        self.state = 'closed'  # closed, open, half-open
+    
+    def call(self, func, *args, **kwargs):
+        if self.state == 'open':
+            if datetime.now() - self.last_failure_time > timedelta(seconds=self.recovery_timeout):
+                self.state = 'half-open'
+            else:
+                raise Exception("Circuit breaker is open")
+        
+        try:
+            result = func(*args, **kwargs)
+            if self.state == 'half-open':
+                self.state = 'closed'
+                self.failure_count = 0
+            return result
+        except Exception as e:
+            self.failure_count += 1
+            self.last_failure_time = datetime.now()
+            if self.failure_count >= self.failure_threshold:
+                self.state = 'open'
+            raise
